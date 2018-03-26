9a80366081e753a9d724edb31a447d9d6217c70a
Jabuk <jakub.klecki@gmail.com>
2018-03-26 22:52:21 +0200

    Splash Screen

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+private AudioSource music;
+public AudioClip introClip;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	music = GetComponent<AudioSource>();
+	music.clip = introClip;
+	music.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
